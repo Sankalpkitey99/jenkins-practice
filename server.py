726b982999b1c70bfab356da5ee6334fee1246b4
Sankalpkitey99 <sankalpkitey002@gmail.com>
2025-06-13 20:16:03 +0530

    update 2.0

--- server.py
+++ server.py
@@ -4,7 +4,7 @@ app = Flask(__name__)  # Correct object creation
 
 @app.route('/', methods=["GET"])
 def root():
-    return "this is my web 0.1"  # Must use 'return' inside the function
+    return "this is my web 0.2"  # Must use 'return' inside the function
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=4000)  # Runs on port 4000
