2c37e42dbe6a1b5cec932d31e723a5588893c981
Sankalpkitey99 <sankalpkitey002@gmail.com>
2025-06-13 19:48:10 +0530

    first commit

---
+++ server.py
@@ -0,0 +1,11 @@
+from flask import Flask  # Capital "F" in Flask
+
+app = Flask(__name__)  # Correct object creation
+
+@app.route('/', methods=["GET"])
+def root():
+    return "this is my web with poll SCM v.07"  # Must use 'return' inside the function
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=4000)  # Runs on port 4000
+
